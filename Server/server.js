b97070c78a1f56b37b5bad8e08870159c1f559c2
Prasathmap <120239478+Prasathmap@users.noreply.github.com>
2024-12-20 11:01:01 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -35,6 +35,7 @@ app.use(cors({
   origin: 'https://mapit-eta.vercel.app',
   credentials: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.get('/', (req, res) => {
