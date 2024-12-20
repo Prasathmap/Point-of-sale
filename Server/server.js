205fd462e3975a7d22d05f84291a61bdeb2da5dd
Prasathmap <120239478+Prasathmap@users.noreply.github.com>
2024-12-21 00:31:38 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -40,7 +40,7 @@ app.use(cors({
 app.options('*', cors());
 app.use((req, res, next) => {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend-domain.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mapit-eta.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
