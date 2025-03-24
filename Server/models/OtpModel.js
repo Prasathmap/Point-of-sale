0ff2a940d358a07ec470593fcf13098278522674
Prasathmap <120239478+Prasathmap@users.noreply.github.com>
2025-03-24 15:28:17 +0530

    Delete Server directory

--- Server/models/OtpModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const otpSchema = new mongoose.Schema({
-  email: { type: String, required: false },
-  emailOtp: { type: String, required: false },
-  expiresAt: { type: Date, required: true },
-});
-
-module.exports = mongoose.model("OTP", otpSchema);
